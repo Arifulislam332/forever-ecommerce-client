43b4b6f1079f8ab972f82469c9568d4273a5918e
Arifulislam332 <ayatullaharif332@gmail.com>
2024-09-30 04:15:14 +0600

    routes navber

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Navber from "./components/Navber";
+
+const App = () => {
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navber />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/placeOrder" element={<PlaceOrder />} />
+        <Route path="/product/:productId" element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
