d556db029cc37fbf8ae34d34a2353abc42063d98
Arifulislam332 <ayatullaharif332@gmail.com>
2024-10-01 23:27:33 +0600

    news letter box section

---
+++ src/components/NewsletterBox.jsx
@@ -0,0 +1,38 @@
+import { Send } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+const NewsletterBox = () => {
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="text-center">
+      <p className="text-2xl font-medium text-gray-700">
+        Subscribe now & get 20% off
+      </p>
+      <p className="text-gray-400 mt-3">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis omnis
+        nemo laboriosam deleniti officia mollitia.
+      </p>
+
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6"
+      >
+        <Input
+          className="w-full sm:flex-1 outline-none border border-gray-200"
+          placeholder="Enter your email"
+          type="email"
+          required
+        />
+        <Button className="uppercase flex gap-2 items-center" type="submit">
+          <Send size={18} />
+          Subscribe
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default NewsletterBox;
